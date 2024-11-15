2cda6a7301c1c56658761b8dc34f5ea2e3a9f684
Fair-Game-TM <157636823+FAIR-GAME-TM@users.noreply.github.com>
2024-11-15 13:33:59 -0700

    Update buildComm.js

--- Fair-Game_Offical-main/buildComm.js
+++ Fair-Game_Offical-main/buildComm.js
@@ -1,20 +1,19 @@
 const fs = require('fs');
-const path = require('path');
+const pathModule = require('path');  // Rename this to avoid conflict
 
-const srcDir = path.join(__dirname, 'Fair-Game_Offical-main');  // Source folder
-const distDir = path.join(__dirname, 'Fair-Game_Offical-main', 'dist');  // Output folder
+const srcDir = pathModule.join(__dirname, 'Fair-Game_Offical-main');  // Source folder
+const distDir = pathModule.join(__dirname, 'Fair-Game_Offical-main', 'dist');  // Output folder
 
-/const fs = require('fs')/;
-const path = './dist';
+const distPath = './dist';  // Rename the variable to avoid conflict with 'pathModule'
 
-if (!fs.existsSync(path)) {
-    fs.mkdirSync(path);
+if (!fs.existsSync(distPath)) {
+    fs.mkdirSync(distPath);
 }
 
 // Copy all files from the source folder to the dist folder (this is just an example)
 fs.readdirSync(srcDir).forEach(file => {
-    const srcFilePath = path.join(srcDir, file);
-    const distFilePath = path.join(distDir, file);
+    const srcFilePath = pathModule.join(srcDir, file);
+    const distFilePath = pathModule.join(distDir, file);
 
     if (fs.lstatSync(srcFilePath).isFile()) {
         fs.copyFileSync(srcFilePath, distFilePath);
