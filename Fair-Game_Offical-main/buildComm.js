0c9b57cf7ad7787057b73f274c2f996cd953a91e
Fair-Game-TM <157636823+FAIR-GAME-TM@users.noreply.github.com>
2024-11-15 13:37:45 -0700

    Update buildComm.js

--- Fair-Game_Offical-main/buildComm.js
+++ Fair-Game_Offical-main/buildComm.js
@@ -1,14 +1,15 @@
+
 const fs = require('fs');
 const pathModule = require('path');
+if (!fs.existsSync(distPath)) {
+    fs.mkdirSync(distPath);
+}
 
 // Adjust paths to reflect the subfolder structure
 const srcDir = pathModule.join(__dirname, 'Fair-Game_Offical-main');  // Source folder
 const distDir = pathModule.join(__dirname, 'Fair-Game_Offical-main', 'dist');  // Output folder
 const distPath = './Fair-Game_Offical-main/dist';  // Directory for output
 
-if (!fs.existsSync(distPath)) {
-    fs.mkdirSync(distPath);
-}
 
 // Copy all files from the source folder to the dist folder
 fs.readdirSync(srcDir).forEach(file => {
